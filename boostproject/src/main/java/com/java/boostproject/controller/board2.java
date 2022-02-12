4e632eb34e93de6c312c6f9ed32945799a051355
samsung <samsung@DESKTOP-AM9CSDQ>
2022-02-12 15:27:53 +0900

    공지사항 삭제 , 연습파일 삭ㅈ

--- boostproject/src/main/java/com/java/boostproject/controller/board2.java
+++ boostproject/src/main/java/com/java/boostproject/controller/board2.java
@@ -3,6 +3,7 @@ package com.java.boostproject.controller;
 public class board2 {
 	public static void main(String[]args) {
 		System.out.println("깃");
+		System.out.println("공지사항 삭제 구현 완료");
 	}
 
 }
